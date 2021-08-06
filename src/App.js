fb423df98b645e20ada76beff774970c20e94639
Amanda Diana <amandadianacmd@gmail.com>
2021-08-06 11:23:44 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Status from "./Status";
 
 function App() {
+  let [messages, setMessages] = useState(["test", "test2"]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <Status onEnter={(value) => setMessage()} />
+        {messages.map((message) => (
+          <li key={message}>{message}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
+x;
 export default App;
---
+++ src/Status.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Status = ({ onEnter }) => {
+  return <></>;
+};
+
+export default Status;
